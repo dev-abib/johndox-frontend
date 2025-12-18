7e83a5fca55ddeedc8a9cd52e34e8b62f7543ab1
alimuzzaman-zamaddar <alimuzzamanzamaddar@gmail.com>
2025-12-18 14:04:54 +0600

    ndone

---
+++ src/Components/Svg/SvgContainer2.tsx
@@ -0,0 +1,35 @@
+export function XIconSvg() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+<path d="M0.75 10.25C0.75 5.77166 0.75 3.53249 2.14124 2.14124C3.53249 0.75 5.77166 0.75 10.25 0.75C14.7283 0.75 16.9675 0.75 18.3588 2.14124C19.75 3.53249 19.75 5.77166 19.75 10.25C19.75 14.7283 19.75 16.9675 18.3588 18.3588C16.9675 19.75 14.7283 19.75 10.25 19.75C5.77166 19.75 3.53249 19.75 2.14124 18.3588C0.75 16.9675 0.75 14.7283 0.75 10.25Z" stroke="white" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.25 15.25L9.44355 11.0565M15.25 5.25L11.0565 9.44355M11.0565 9.44355L8.02778 5.25H5.25L9.44355 11.0565M11.0565 9.44355L15.25 15.25H12.4722L9.44355 11.0565" stroke="white" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  );
+}
+export function InIconSvg() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+<path d="M5.25 8.25V15.25" stroke="white" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M9.25 11.25V15.25M9.25 11.25C9.25 9.59315 10.5931 8.25 12.25 8.25C13.9069 8.25 15.25 9.59315 15.25 11.25V15.25M9.25 11.25V8.25" stroke="white" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M5.25801 5.25L5.24902 5.25" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M0.75 10.25C0.75 5.77166 0.75 3.53249 2.14124 2.14124C3.53249 0.75 5.77166 0.75 10.25 0.75C14.7283 0.75 16.9675 0.75 18.3588 2.14124C19.75 3.53249 19.75 5.77166 19.75 10.25C19.75 14.7283 19.75 16.9675 18.3588 18.3588C16.9675 19.75 14.7283 19.75 10.25 19.75C5.77166 19.75 3.53249 19.75 2.14124 18.3588C0.75 16.9675 0.75 14.7283 0.75 10.25Z" stroke="white" strokeWidth="1.5" stroke-linejoin="round"/>
+</svg>
+  );
+}
+export function InstaSvg() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="21" height="21" viewBox="0 0 21 21" fill="none">
+<path d="M0.75 10.25C0.75 5.77166 0.75 3.53249 2.14124 2.14124C3.53249 0.75 5.77166 0.75 10.25 0.75C14.7283 0.75 16.9675 0.75 18.3588 2.14124C19.75 3.53249 19.75 5.77166 19.75 10.25C19.75 14.7283 19.75 16.9675 18.3588 18.3588C16.9675 19.75 14.7283 19.75 10.25 19.75C5.77166 19.75 3.53249 19.75 2.14124 18.3588C0.75 16.9675 0.75 14.7283 0.75 10.25Z" stroke="white" strokeWidth="1.5" stroke-linejoin="round"/>
+<path d="M14.75 10.25C14.75 12.7353 12.7353 14.75 10.25 14.75C7.76472 14.75 5.75 12.7353 5.75 10.25C5.75 7.76472 7.76472 5.75 10.25 5.75C12.7353 5.75 14.75 7.76472 14.75 10.25Z" stroke="white" strokeWidth="1.5"/>
+<path d="M15.7578 4.75L15.7488 4.75" stroke="white" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  );
+}
+export function FbSvg() {
+  return (
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+<path d="M14 9.3V12.2H16.6C16.8 12.2 16.9 12.4 16.9 12.6L16.5 14.5C16.5 14.6 16.3 14.7 16.2 14.7H14V22H11V14.8H9.3C9.1 14.8 9 14.7 9 14.5V12.6C9 12.4 9.1 12.3 9.3 12.3H11V9C11 7.3 12.3 6 14 6H16.7C16.9 6 17 6.1 17 6.3V8.7C17 8.9 16.9 9 16.7 9H14.3C14.1 9 14 9.1 14 9.3Z" stroke="white" strokeWidth="1.5" stroke-miterlimit="10" stroke-linecap="round"/>
+<path d="M15 22H9C4 22 2 20 2 15V9C2 4 4 2 9 2H15C20 2 22 4 22 9V15C22 20 20 22 15 22Z" stroke="white" strokeWidth="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+  );
+}
