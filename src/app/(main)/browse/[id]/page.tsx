19028af94bfde7673bb3776d9f96c7cb39238b7c
fahim-sarker <sarkerfahim599@gmail.com>
2025-12-19 16:55:45 +0600

    complete the description section

--- src/app/(main)/browse/[id]/page.tsx
+++ src/app/(main)/browse/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BrowseDetails from "@/Components/PageComponents/mainPages/BrowseDetails/BrowseDetails";
+import BrowseDescription from "@/Components/PageComponents/mainPages/BrowseDetails/BrowseDescription";
 
 const page = () => {
   return (
     <>
       <BrowseDetails />
+      <BrowseDescription />
     </>
   );
 };
