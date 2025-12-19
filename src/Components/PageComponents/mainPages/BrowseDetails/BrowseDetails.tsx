5d19f82e1de6fa5a56eb21333aae93b5fe9059ef
fahim-sarker <sarkerfahim599@gmail.com>
2025-12-19 16:25:14 +0600

    details header complete

--- src/Components/PageComponents/mainPages/BrowseDetails/BrowseDetails.tsx
+++ src/Components/PageComponents/mainPages/BrowseDetails/BrowseDetails.tsx
@@ -1,8 +1,21 @@
 "use client";
+import Image from "next/image";
 import React, { useRef, useState } from "react";
+import User from "../../../../Assets/user.png";
 import Container from "@/Components/Common/Container";
-import { Save, Video } from "@/Components/Svg/SvgContainer";
 import { IoShareSocialOutline } from "react-icons/io5";
+import {
+  Acceleration,
+  Bathtub,
+  Bed,
+  Converter,
+  Email,
+  Location,
+  Mobile,
+  Save,
+  Star,
+  Video,
+} from "@/Components/Svg/SvgContainer";
 
 const BrowseDetails: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
@@ -61,12 +74,85 @@ const BrowseDetails: React.FC = () => {
                   <IoShareSocialOutline className="text-[#0085FF]" />
                 </div>
               </div>
-              <div className="flex gap-x-10">
-                <h4 className="font-semibold text-[28px] text-[#0085FF] shrink-0">
-                  $ 10,000
-                </h4>
-                <Save className="shrink-0" />
+              <div className="">
+                <div className="flex gap-x-10">
+                  <h4 className="font-semibold text-[28px] text-[#0085FF] shrink-0">
+                    $ 10,000
+                  </h4>
+                  <div className="flex flex-col gap-6">
+                    <Save className="shrink-0" />
+                    <Converter />
+                  </div>
+                </div>
               </div>
+            </div>
+            <div className="pt-5 pb-8">
+              <div className="flex items-center gap-2.5 mt-4">
+                <Location />
+                <p className="text-base xl:text-[16px] font-medium text-[#404040]">
+                  Caldas, Honduras
+                </p>
+              </div>
+              <div className="flex flex-wrap gap-5 mt-5">
+                <div className="flex items-center gap-2.5">
+                  <Bed />
+                  <span className="text-sm lg:text-[14px] font-normal text-[#404040]">
+                    3 Beds
+                  </span>
+                </div>
+                <div className="flex items-center gap-2.5">
+                  <Bathtub />
+                  <span className="text-sm lg:text-[14px] font-normal text-[#404040]">
+                    2 Baths
+                  </span>
+                </div>
+                <div className="flex items-center gap-2.5">
+                  <Acceleration />
+                  <span className="text-sm lg:text-[16px] font-normal text-[#404040]">
+                    2200 M
+                  </span>
+                </div>
+              </div>
+            </div>
+            <div className="">
+              <h5 className="text-[#101010] text-[24px] font-medium ">
+                AGENT INFORMATION
+              </h5>
+              <div className="flex gap-x-5 mt-5 items-end">
+                <figure>
+                  <Image src={User} alt="User" width={70} height={70} />
+                </figure>
+                <ul className="flex flex-col gap-1">
+                  <li className="text-[24px] font-medium text-[#0085FF]">
+                    Jessy King
+                  </li>
+                  <li className="text-[18px] font-medium text-[#5F5F5F]">
+                    Senior Real Estate Agent
+                  </li>
+                  <li className="flex gap-x-2 text-[18px] font-medium text-[#5F5F5F]">
+                    <Star />
+                    4.8 (127 review)
+                  </li>
+                </ul>
+                <ul className="flex flex-col gap-1">
+                  <li className="flex gap-x-2 text-[18px] font-medium text-[#5F5F5F]">
+                    <Mobile />
+                    +52 384 123 4568
+                  </li>
+                  <li className="flex gap-x-2 text-[18px] font-medium text-[#5F5F5F]">
+                    <Email />
+                    jessayking@gmail.com
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex gap-x-7 mt-8">
+              <button className="w-full bg-[#0085FF] text-white font-medium text-base lg:text-lg py-2 xl:py-3 rounded-2xl hover:bg-transparent hover:text-[#0085FF] border border-[#0085FF] transition-all duration-300 cursor-pointer">
+                Request a tour
+              </button>
+              <button className="w-full hover:bg-[#0085FF] hover:text-white font-medium text-base lg:text-lg py-2 xl:py-3 rounded-2xl bg-transparent text-[#0085FF] border border-[#0085FF] transition-all duration-300 cursor-pointer">
+                Message
+              </button>
             </div>
           </div>
         </div>
