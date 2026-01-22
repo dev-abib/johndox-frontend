6046e83852a1fc2cbb823946320f681237b5d5a7
fahim-sarker <sarkerfahim599@gmail.com>
2026-01-22 12:39:54 +0600

    feat:fixed the alignment

--- src/app/(main)/forseller/page.tsx
+++ src/app/(main)/forseller/page.tsx
@@ -1,14 +1,19 @@
 "use client";
+import Forsellerskeleton from "@/Components/Skeleton/Forsellerskeleton";
+import WhySell from "@/Components/PageComponents/mainPages/forseller/WhySell";
+import { forSellerBanner, useWhyChoose, whyitWorks } from "@/Hooks/api/cms_api";
 import HowItWorks from "@/Components/PageComponents/mainPages/forseller/HowItWorks";
 import ReadytoSell from "@/Components/PageComponents/mainPages/forseller/ReadytoSell";
 import SellerPageHero from "@/Components/PageComponents/mainPages/forseller/SellerPageHero";
-import WhySell from "@/Components/PageComponents/mainPages/forseller/WhySell";
-import { forSellerBanner, useWhyChoose, whyitWorks } from "@/Hooks/api/cms_api";
 
 export default function Page() {
   const { data: hero } = forSellerBanner();
   const { data: whyitworks } = whyitWorks();
-  const { data, isLoading, error } = useWhyChoose();
+  const { data, isLoading } = useWhyChoose();
+
+  if (isLoading) {
+    return <Forsellerskeleton />;
+  }
   return (
     <div>
       <SellerPageHero hero={hero} />
